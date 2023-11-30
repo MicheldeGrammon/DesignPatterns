15329b8ab110b9bd8b5199f79047d37bcbc2b08a
Brasukov Alex <110027920+MicheldeGrammon@users.noreply.github.com>
2023-11-30 10:43:06 +0500

    Add Singleton

--- CreationalPatterns/Singleton/Program.cs
+++ CreationalPatterns/Singleton/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var worker1 = Worker.GetInstance("worker1");
+            Console.WriteLine($"Worker1\nName: {worker1.Name}, Time: {worker1.TimeInstance}");
+
+            Thread.Sleep( 2000 );
+            Console.WriteLine();
+
+            var worker2 = Worker.GetInstance("worker2");
+            Console.WriteLine($"Worker2\nName: {worker2.Name}, Time: {worker2.TimeInstance}");
         }
     }
 }
---
+++ CreationalPatterns/Singleton/Worker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Worker
+    {
+
+        private static Worker instance;
+        public string Name { get; private set; }
+        public DateTime TimeInstance { get; private set; }
+
+        private Worker(string name)
+        {
+            Name = name;
+            TimeInstance = DateTime.Now;
+        }
+
+        public static Worker GetInstance(string name)
+        {
+            if (instance == null)
+                instance = new Worker(name);
+
+            return instance;
+        }
+    }
+}
